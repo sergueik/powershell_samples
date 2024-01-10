6079b7558c51e8ce561d73cff82dd7b6bde07b2f
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2024-01-11 00:04:16 +0100

    intermediate revision - added fallback logic

--- fp.pl
+++ fp.pl
@@ -5,8 +5,34 @@ use strict;
 # Origin: TPL
 my $funcs = {
     'x' => sub { sprintf 'xxx %s', shift },
-    'y' => sub { sprintf 'yyy %d', shift },
+    'y' => sub { my $value = shift; sprintf 'yyy %d', $value },
 };
+my $fallback = sub { sprintf 'default %s', shift };
+sub fallback_method {
+  sprintf 'default %s', shift 
+}
 my $data = { 'x' => 10, 'y' => 20 };
 map { print $_ . ': ' . $funcs->{$_}->( $data->{$_} ) . $/ } keys %$data;
+$data->{'z'} = 100;
+my $k;
+my $v;
 
+foreach $k ( keys %$data ) {
+    if ( exists $$funcs{$k} ) {
+        $v = $funcs->{$k}->( $data->{$k} );
+    }
+    else {
+        $v = $fallback->( $data->{$k} );
+    }
+    print $v, $/;
+}
+
+foreach my $key ( keys %$data ) {
+    my $arg = $data->{$key};
+    my $val =
+      ( exists $$funcs{$key} )
+      ? $funcs->{$key}->($arg)
+      : $fallback->($arg);
+
+    print $val, $/;
+}
