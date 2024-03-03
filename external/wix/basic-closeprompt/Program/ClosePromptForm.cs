527a7e8463491f0ef6315f0377c67b653f503a8f
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2024-03-03 09:06:22 -0800

    intermediate revision - added close prompt example

---
+++ external/wix/basic-closeprompt/Program/ClosePromptForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ClosePromptCA {
+	public partial class ClosePromptForm : Form {
+		public ClosePromptForm(string text) {
+			InitializeComponent();
+			messageText.Text = text;
+		}
+
+		private void OkButtonClick(object sender, EventArgs e) {
+			DialogResult = DialogResult.OK;
+			Close();
+		}
+	}
+}
