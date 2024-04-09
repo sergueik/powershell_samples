325f1e9bfa41aa44f6795c6d4d8b74bb9d1cafc2
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2024-04-10 01:26:12 +0200

    intermedite revision -  updated the behavior with "rotate_message" function to illustrate usage for hierarchical data sets, e.g. performance counters, and performance counter categories, and somewhat retty printed 10 year old code

--- check_item_list.ps1
+++ check_item_list.ps1
@@ -1,4 +1,4 @@
-#Copyright (c) 2014 Serguei Kouzmine
+#Copyright (c) 2014,2024 Serguei Kouzmine
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
@@ -18,6 +18,15 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #THE SOFTWARE.
 
+function rotate_message {
+  param(
+    [string] $s1 = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
+    [string] $s2 = 'ipsum'
+  )
+  $s3 = @($s1 -split $s2)
+  # TODO: check if found
+  return $s2 + $s3[1] + ' ' + $s3[0]
+}
 Add-Type -TypeDefinition @"
 
 // "
@@ -60,7 +69,7 @@ function PromptCheckedList
      Param(
 	[String] $title, 
 	[String] $message)
-
+  [string]$rotated_message = ''
   [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Drawing') 
   [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Collections.Generic') 
   [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Collections') 
@@ -68,7 +77,7 @@ function PromptCheckedList
   [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
   [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Text') 
   [void] [System.Reflection.Assembly]::LoadWithPartialName('System.Data') 
-  $f = New-Object System.Windows.Forms.Form 
+  $f = new-object System.Windows.Forms.Form 
   $f.Text = $title
 
   $i = new-object System.Windows.Forms.CheckedListBox
@@ -77,12 +86,12 @@ function PromptCheckedList
   $i.SuspendLayout()
   $f.SuspendLayout()
   $i.Font = new-object System.Drawing.Font('Microsoft Sans Serif', 11, [System.Drawing.FontStyle]::Regular, [System.Drawing.GraphicsUnit]::Point, 0);
-  $i.FormattingEnabled = $true;
-  $i.Items.AddRange(( $message -split '[ ,]+' ));
+  $i.FormattingEnabled = $true
+  $i.Items.AddRange(( $message -split '[ ,]+' ))
 
-  $i.Location = New-Object System.Drawing.Point(17, 12)
+  $i.Location = new-object System.Drawing.Point(17, 12)
   $i.Name = 'inputCheckedListBox'
-  $i.Size = New-Object System.Drawing.Size(202, 188)
+  $i.Size = new-object System.Drawing.Size(202, 188)
   $i.TabIndex = 0
   $i.TabStop = $false
   $event_handler = {  
@@ -91,26 +100,30 @@ function PromptCheckedList
             [System.Windows.Forms.ItemCheckEventArgs ] $eventargs 
          )
          $item = $i.SelectedItem
-         if ( $eventargs.NewValue -eq  [System.Windows.Forms.CheckState]::Checked ) { 
-            $d.Items.Add( $item );
+         # NOTE: need a global to keep the items
+         $rotated_message = rotate_message -s2 $item -s1 $message
+
+         if ( $eventargs.NewValue -eq  [System.Windows.Forms.CheckState]::Checked ) {
+
+            $d.Items.Add( $rotated_message)
          } else {
-            $d.Items.Remove( $item );
+            $d.Items.Remove( $rotated_message )
          }
   }
   $i.Add_ItemCheck($event_handler) 
 
-  $d.Font = New-Object System.Drawing.Font('Verdana', 11)
+  $d.Font = new-object System.Drawing.Font('Verdana', 11)
   $d.FormattingEnabled = $true
-  $d.ItemHeight = 20;
-  $d.Location =  New-Object System.Drawing.Point(236, 12);
-  $d.Name = 'displayListBox';
-  $d.Size = New-Object System.Drawing.Size(190, 184);
-  $d.TabIndex = 1;
+  $d.ItemHeight = 20
+  $d.Location =  new-object System.Drawing.Point(236, 12)
+  $d.Name = 'displayListBox'
+  $d.Size = new-object System.Drawing.Size(190, 184)
+  $d.TabIndex = 1
 
-  $b  = New-Object System.Windows.Forms.Button
-  $b.Location = New-Object System.Drawing.Point(8, 280)
+  $b  = new-object System.Windows.Forms.Button
+  $b.Location = new-object System.Drawing.Point(8, 280)
   $b.Name = 'button1'
-  $b.Size = New-Object System.Drawing.Size(112, 32)
+  $b.Size = new-object System.Drawing.Size(112, 32)
   $b.TabIndex = 4
   $b.Text = 'Done'
 
@@ -124,9 +137,9 @@ function PromptCheckedList
     $f.Close()
  })
      
-  $f.AutoScaleBaseSize = New-Object System.Drawing.Size(5, 13)
-  $f.ClientSize = New-Object System.Drawing.Size(408, 317)
-  $components =  New-Object System.ComponentModel.Container
+  $f.AutoScaleBaseSize = new-object System.Drawing.Size(5, 13)
+  $f.ClientSize = new-object System.Drawing.Size(408, 317)
+  $components =  new-object System.ComponentModel.Container
 
   $f.Controls.AddRange( @( $i, $d, $b))
 
@@ -142,7 +155,7 @@ function PromptCheckedList
   $f.KeyPreview = $True
 
   $f.Topmost = $True
-  $caller = New-Object Win32Window -ArgumentList ([System.Diagnostics.Process]::GetCurrentProcess().MainWindowHandle)
+  $caller = new-object Win32Window -ArgumentList ([System.Diagnostics.Process]::GetCurrentProcess().MainWindowHandle)
 
   $f.Add_Shown( { $f.Activate() } )
 
@@ -156,4 +169,4 @@ function PromptCheckedList
 $DebugPreference = 'Continue'
 $result = PromptCheckedList ''  'Lorem ipsum dolor sit amet, consectetur adipisicing elit' 
 
-write-debug ('Selection is : {0}' -f  , $result )
+# write-debug ('Selection is : {0}' -f  , $result )
