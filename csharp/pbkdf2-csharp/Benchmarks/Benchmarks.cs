b1b8763188acb6384cac9ca3b1353aaf7f112837
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2025-08-13 12:56:21 -0700

    intermediate revision - downgtared to older version of "BenchmarkDotNet", 0.9.8, still no use of the Benchmarks subproject

--- csharp/pbkdf2-csharp/Benchmarks/Benchmarks.cs
+++ csharp/pbkdf2-csharp/Benchmarks/Benchmarks.cs
@@ -1,13 +1,11 @@
 using System;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
+using Program;
 
-namespace Benchmarks
-{
-	public class PbkdfTest
-	{
-		private const int N = 10000;
-		private readonly byte[] data;
+
+namespace Benchmarks {
+	public class PbkdfTest {
 
 		private static String plainValue;
 		private static String encryptedValue;
@@ -15,33 +13,29 @@ namespace Benchmarks
 		private static String password;
 		private static bool debug;
 
-		public PbkdfTest()
-		{
+		public PbkdfTest() {
 			saltString = "fec4c9acd8c72cd9790ccfb953ba48f7";
 			password = "password";
 			debug = true;
 			plainValue = "test";
 			encryptedValue = "/sTJrNjHLNl5DM+5U7pI98y1r5JDXiW2cfCLVjou2AcgYo+wgjLTzCGWuMA9dCC2";
 			Program.Program.Debug = debug;
-
 		}
 
 		[Benchmark]
 		// TODO: engage .net core syntax
-        // public string  Program() => program.Encrypt(plainValue, password, saltSting);
-	public string Encrypt()
-		{ 
+        	// public string  Program() => program.Encrypt(plainValue, password, saltSting);
+		public string Encrypt() {
 			var value = Program.Program.Encrypt(plainValue, password, saltString);
 			// read  salt and iv from payload
 			var payload = Convert.FromBase64String(value);
-
+			return null;
 		}
 
-		public class Program {
+		public class Example {
 			public static void Main(string[] args) {
-				var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+				var summary = BenchmarkRunner.Run(typeof(Example));
 			}
 		}
-
 	}
 }
