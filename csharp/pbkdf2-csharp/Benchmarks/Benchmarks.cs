c15fab52e4c667468e93530a4c6155d653eaaa3f
Serguei Kozumine <kouzmine_serguei@yahoo.com>
2025-08-13 15:28:37 -0400

    intermediate revision - adding benchmarks, not working yet

---
+++ csharp/pbkdf2-csharp/Benchmarks/Benchmarks.cs
@@ -0,0 +1,47 @@
+using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Benchmarks
+{
+	public class PbkdfTest
+	{
+		private const int N = 10000;
+		private readonly byte[] data;
+
+		private static String plainValue;
+		private static String encryptedValue;
+		private static String saltString;
+		private static String password;
+		private static bool debug;
+
+		public PbkdfTest()
+		{
+			saltString = "fec4c9acd8c72cd9790ccfb953ba48f7";
+			password = "password";
+			debug = true;
+			plainValue = "test";
+			encryptedValue = "/sTJrNjHLNl5DM+5U7pI98y1r5JDXiW2cfCLVjou2AcgYo+wgjLTzCGWuMA9dCC2";
+			Program.Program.Debug = debug;
+
+		}
+
+		[Benchmark]
+		// TODO: engage .net core syntax
+        // public string  Program() => program.Encrypt(plainValue, password, saltSting);
+	public string Encrypt()
+		{ 
+			var value = Program.Program.Encrypt(plainValue, password, saltString);
+			// read  salt and iv from payload
+			var payload = Convert.FromBase64String(value);
+
+		}
+
+		public class Program {
+			public static void Main(string[] args) {
+				var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+			}
+		}
+
+	}
+}
