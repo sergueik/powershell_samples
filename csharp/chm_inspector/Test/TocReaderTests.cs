4d273f2550c39db0b8ef96f492cd936a00071cbd
Serguei Kozumine <kouzmine_serguei@yahoo.com>
2025-11-17 00:33:25 -0500

    intermediate revision - added toc.hhc that is causing timeout to regex based parseToc, added nunit 2.6.3 syntax timeout annotation

--- csharp/chm_inspector/Test/TocReaderTests.cs
+++ csharp/chm_inspector/Test/TocReaderTests.cs
@@ -13,14 +13,14 @@ namespace Tests {
 		public void test1() {
 			var toclist = Chm.toc_structured(file);
 
-            // Assert
-            Assert.IsNotNull(toclist, "The toclist list should not be null");
-            Assert.IsNotEmpty(toclist, "The toclist list should not be empty");
+			// Assert
+			Assert.IsNotNull(toclist, "The toclist list should not be null");
+			Assert.IsNotEmpty(toclist, "The toclist list should not be empty");
 
-            foreach (var entry in toclist) {
-                Assert.IsFalse(string.IsNullOrEmpty(entry.Name), "Entry Name should not be null or empty");
-                Assert.IsFalse(string.IsNullOrEmpty(entry.Local), "Entry Local should not be null or empty");
-                Console.Error.WriteLine("{0}: {1}", entry.Name, entry.Local);
+			foreach (var entry in toclist) {
+			    Assert.IsFalse(string.IsNullOrEmpty(entry.Name), "Entry Name should not be null or empty");
+			    Assert.IsFalse(string.IsNullOrEmpty(entry.Local), "Entry Local should not be null or empty");
+			    Console.Error.WriteLine("{0}: {1}", entry.Name, entry.Local);
             }
 		}
 
@@ -29,28 +29,28 @@ namespace Tests {
 			Dictionary<string,string> toc = Chm.tocdict_7zip(file);
 
 			// Assert
-            Assert.IsNotNull(toc, "The dictionary should not be null");
-            Assert.IsNotEmpty(toc, "The dictionary should not be empty");
-
-            foreach (var keyValuePair in toc) {
-                Assert.IsFalse(string.IsNullOrEmpty(keyValuePair.Key), "Key (Name) should not be null or empty");
-                Assert.IsFalse(string.IsNullOrEmpty(keyValuePair.Value), "Value (Local) should not be null or empty");
-                Console.Error.WriteLine("{0}: {1}", keyValuePair.Key, keyValuePair.Value);
-            }		
+			Assert.IsNotNull(toc, "The dictionary should not be null");
+			Assert.IsNotEmpty(toc, "The dictionary should not be empty");
+			
+			foreach (var keyValuePair in toc) {
+			    Assert.IsFalse(string.IsNullOrEmpty(keyValuePair.Key), "Key (Name) should not be null or empty");
+			    Assert.IsFalse(string.IsNullOrEmpty(keyValuePair.Value), "Value (Local) should not be null or empty");
+			    Console.Error.WriteLine("{0}: {1}", keyValuePair.Key, keyValuePair.Value);
+			}		
 		}
 		[Test]
 		public void test3() {
 			var toclist = Chm.toc_7zip(file);
-
-            // Assert
-            Assert.IsNotNull(toclist, "The toclist list should not be null");
-            Assert.IsNotEmpty(toclist, "The toclist list should not be empty");
-
-            foreach (var entry in toclist) {
-                Assert.IsFalse(string.IsNullOrEmpty(entry.Name), "Entry Name should not be null or empty");
-                Assert.IsFalse(string.IsNullOrEmpty(entry.Local), "Entry Local should not be null or empty");
-                Console.Error.WriteLine("{0}: {1}", entry.Name, entry.Local);
-            }
+			
+			// Assert
+			Assert.IsNotNull(toclist, "The toclist list should not be null");
+			Assert.IsNotEmpty(toclist, "The toclist list should not be empty");
+			
+			foreach (var entry in toclist) {
+			    Assert.IsFalse(string.IsNullOrEmpty(entry.Name), "Entry Name should not be null or empty");
+			    Assert.IsFalse(string.IsNullOrEmpty(entry.Local), "Entry Local should not be null or empty");
+			    Console.Error.WriteLine("{0}: {1}", entry.Name, entry.Local);
+			}
 		}
 	}
 }
