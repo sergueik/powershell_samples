71e22bb7d128c84738d32f804eb1edfbac4c742e
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2026-01-15 00:13:48 -0500

    intermediate revision - made signatures aligned

--- external/csharp/basic-ascii-ebcdic/code/Program.cs
+++ external/csharp/basic-ascii-ebcdic/code/Program.cs
@@ -4,34 +4,27 @@ using System.Text;
 namespace ASCII_EBCIDIC_Converter {
 	internal class Program {
 
-		public static string ConvertEbcdicToAscii(byte[] ebcdicBytes, int codePage) {
-			Encoding ebcdicEncoding = Encoding.GetEncoding(codePage);
-			string unicodeString = ebcdicEncoding.GetString(ebcdicBytes);
-			byte[] asciiBytes = Encoding.ASCII.GetBytes(unicodeString);
-			return Encoding.ASCII.GetString(asciiBytes);
+		public static byte[] ConvertEbcdicToAscii(byte[] ebcdicBytes, string codePage) {
+			byte[] asciiBytes = Encoding.ASCII.GetBytes(Encoding.GetEncoding(codePage).GetString(ebcdicBytes));
+			return asciiBytes;
 		}
 
-		public static byte[] ConvertAsciiToEbcdic(string asciiString, int codePage) {
-			Encoding asciiEncoding = Encoding.ASCII;
-			Encoding ebcdicEncoding = Encoding.GetEncoding(codePage);
-			byte[] asciiBytes = asciiEncoding.GetBytes(asciiString);
-			byte[] ebcdicBytes = Encoding.Convert(asciiEncoding, ebcdicEncoding, asciiBytes);
+		public static byte[] ConvertAsciiToEbcdic(byte[] asciiBytes, string codePage) {
+			byte[] ebcdicBytes = Encoding.Convert(Encoding.ASCII, Encoding.GetEncoding(codePage), asciiBytes);
 			return ebcdicBytes;
 		}
 
 		public static void Main(string[] args) {
 
-			string originalAscii = "0123456789abcdefghijklmnopqrstuvwxyz";
-			int ibm037CodePage = 37; // Standard US EBCDIC code page
+			string data = "0123456789abcdefghijklmnopqrstuvwxyz";
+			const string codePage = "IBM037"; // Standard US EBCDIC code page
 
-			byte[] ebcdicData = ConvertAsciiToEbcdic(originalAscii, ibm037CodePage);
-			// Console.WriteLine("EBCDIC bytes (hex): " + BitConverter.ToString(ebcdicData));
+			byte[] ebcdicBytes = ConvertAsciiToEbcdic(Encoding.ASCII.GetBytes(data), codePage);
 			// https://learn.microsoft.com/en-us/dotnet/api/system.bitconverter?view=netframework-4.5
-			Console.WriteLine("EBCDIC bytes (hex): " + BitConverter.ToString(ebcdicData).Replace("-", string.Empty));
+			Console.WriteLine("EBCDIC bytes (hex): " + BitConverter.ToString(ebcdicBytes).Replace("-", string.Empty));
 
-			string convertedAscii = ConvertEbcdicToAscii(ebcdicData, ibm037CodePage);
-			Console.WriteLine(String.Format("Converted back to ASCII: {0}", convertedAscii));
-
-		}
+			byte[] asciiBytes = ConvertEbcdicToAscii(ebcdicBytes, codePage);
+			Console.WriteLine(String.Format("Converted back to ASCII: {0}", Encoding.ASCII.GetString(asciiBytes)));
+			}
 	}
 }
