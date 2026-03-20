e842a40289c16333b6e8826f48353297041ece42
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2026-03-20 08:42:15 -0400

    intermediate revision - added more tests

--- csharp/basic-ascii-ebcdic/Test/ValidatorTests.cs
+++ csharp/basic-ascii-ebcdic/Test/ValidatorTests.cs
@@ -7,50 +7,49 @@ using Utils;
 /**
  * Copyright 2026 Serguei Kouzmine
  */
-namespace Test
-{
+namespace Test {
+
 	[TestFixture]
-	public class ValidatorTests
-	{
+	public class ValidatorTests	{
 		private StringBuilder verificationErrors = new StringBuilder();
 		private static bool debug;
 		private double threshold = .95;
+
 		[SetUp]
-		public void setUp()
-		{
+		public void setUp() {
 			debug = true;
 			verificationErrors.Clear();
 		}
 
 		[TearDown]
-		public void tearDown()
-		{
+		public void tearDown() {
 			Assert.AreEqual("", verificationErrors.ToString());
 		}
 
 		[Test]
-		public void test1()
-		{
+		public void test1() {
 			object[,] arguments = {
-				{ "uppercase HELLO", "C8C5D3D3D6", true },
-				{ "lowercase hello", "8885939396", true },
-				{ "digits 12345", "F1F2F3F4F5", true },
-				{ "HELLO.HELLO punctuation", "C8C5D3D3D64BC8C5D3D3D6", true },
-				{ "contains NULL byte", "C8C500D3D6", false },
-				{ "control characters 0x15", "151515", false },
-				{ "mixed valid and invalid", "C8C5D315D6", false },
-				{ "UTF-8 string 'HELLO'", "48454C4C4F", false },
-				{ "ASCII digits '12345'", "3132333435", false },
-				{ "UTF-16BE 'HELLO'", "00480045004C004C004F", false },
-				{ "UTF-16 BOM + text", "FEFF00480045004C004C004F", false },
-				{ "UTF-8 string 'é' (C3 A9)", "C3A9", true }
+				{ "uppercase HELLO", "C8C5D3D3D6", "IBM037", true },
+				{ "lowercase hello", "8885939396", "IBM037", true },
+				{ "digits 12345", "F1F2F3F4F5", "IBM037", true },
+				{ "HELLO.HELLO punctuation", "C8C5D3D3D64BC8C5D3D3D6", "IBM037", true },
+				{ "contains NULL byte", "C8C500D3D6", "IBM037", false },
+				{ "control characters 0x15", "151515", "IBM037", false },
+				{ "mixed valid and invalid", "C8C5D315D6", "IBM037", false },
+				{ "UTF-8 string 'HELLO'", "48454C4C4F", "IBM037", false },
+				{ "ASCII digits '12345'", "3132333435", "IBM037", false },
+				{ "UTF-16BE 'HELLO'", "00480045004C004C004F", "IBM037", false },
+				{ "UTF-16 BOM + text", "FEFF00480045004C004C004F", "IBM037", false },
+				{ "cp037 digits 12345 claimed to be utf-8", "F1F2F3F4F5", "utf-8", false},
+				{ "cp037 digits 12345 claimed to be ascii", "F1F2F3F4F5", "ascii", false},
+				{ "UTF-8 string 'é' (C3 A9)", "C3A9", "IBM037", true }
 			};
 
 			for (int i = 0; i < arguments.GetLength(0); i++) {
 				string comment = (string)arguments[i, 0];
 				string input = (string)arguments[i, 1];
-				bool expected = (bool)arguments[i, 2];
-				string codePage = "IBM037";
+				string codePage = (string)arguments[i, 2];
+				bool expected = (bool)arguments[i, 3];
 				try {
 					// NOTE: 0x3F is a question mark and if it is logged in the message hex, the string to byte conversion was wrong 
 					byte[] data = Convertor.HexStringToByteArray(input);
