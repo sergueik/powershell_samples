2725f65b0dabcf77991673f2e335349798e87e2d
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2026-03-29 01:11:05 -0400

    intermediate revision - added ProcessInfo, for per process Instance Working Set counter (unfinished)

---
+++ csharp/basic-countermonitor/Test/ProcessInfoTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using System.Diagnostics;
+using System.Linq;
+using System.Collections;
+using System.Reflection;
+using Utils;
+
+namespace Test
+{
+	[TestFixture]
+	public class ProcessInfoTest
+	{
+		private string result = null;
+		[Test]
+		public void test1()
+		{
+			var pid = 20340;
+			result = Utils.ProcessInfo.getProcessInstanceName(pid);
+			Assert.IsNotNull(result);
+			Console.Error.WriteLine(result);
+			StringAssert.IsMatch(@"VirtualBox(?:#\d+)?", result);
+			foreach (var c in new PerformanceCounterCategory("Process").GetCounters("explorer")) {
+				Console.WriteLine(c.CounterName);
+			}
+			var cat = new PerformanceCounterCategory("Process");
+
+			foreach (string counter in cat.GetCounters("_Total").Select(c => c.CounterName)) {
+				Console.WriteLine(counter);
+			}
+		}
+	}
+	
+}
---
+++ csharp/basic-countermonitor/Utils/ProcessInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Utils {
+	public class ProcessInfo {
+		public static string getProcessInstanceName(int pid) {
+			var performanceCounterCategory =
+				new PerformanceCounterCategory("Process");
+
+			foreach (string instance in performanceCounterCategory.GetInstanceNames()) {
+				using (var performanceCounter = new PerformanceCounter("Process", "ID Process", instance, true)) {
+					int rawValue = (int)performanceCounter.RawValue;
+
+					if (rawValue == pid)
+						return instance;
+				}
+			}
+
+			return null;
+		}
+	}
+}
