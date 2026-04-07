bc8df43094f420e9f789180df67d97435f19731d
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2026-04-07 00:13:52 -0400

    intermediate revision - added Client button action for "discover"

--- csharp/basic-countermonitor/Client/Form1.Designer.cs
+++ csharp/basic-countermonitor/Client/Form1.Designer.cs
@@ -10,12 +10,13 @@ using System.ComponentModel;
 namespace Program {
 
 	public partial class Form1 : Form {
-		private TextBox txtCustID;
-		private TextBox txtCountry;
+		private TextBox textbox1;
+		private TextBox textbox2;
 		private Label label1;
 		private Label label4;
+		private Button button1;
 
-		private System.ComponentModel.IContainer components = null;
+		private IContainer components = null;
 
 		protected override void Dispose(bool disposing) {
 			if (disposing) {
@@ -28,52 +29,67 @@ namespace Program {
 
 		#region Windows Form Designer generated code
 		private void InitializeComponent() {
-			this.txtCustID = new System.Windows.Forms.TextBox();
-			this.txtCountry = new System.Windows.Forms.TextBox();
+			this.textbox1 = new System.Windows.Forms.TextBox();
+			this.textbox2 = new System.Windows.Forms.TextBox();
 			this.label1 = new System.Windows.Forms.Label();
 			this.label4 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
-			// 
-			// txtCustID
-			// 
-			this.txtCustID.Location = new System.Drawing.Point(245, 95);
-			this.txtCustID.Name = "txtCustID";
-			this.txtCustID.Size = new System.Drawing.Size(180, 29);
-			this.txtCustID.TabIndex = 0;
-			// 
-			// txtCountry
-			// 
-			this.txtCountry.Location = new System.Drawing.Point(245, 352);
-			this.txtCountry.Name = "txtCountry";
-			this.txtCountry.Size = new System.Drawing.Size(317, 29);
-			this.txtCountry.TabIndex = 3;
-			// 
+			//
+			// textbox1
+			//
+			this.textbox1.Location = new System.Drawing.Point(245, 82);
+			this.textbox1.Name = "textbox1";
+			this.textbox1.Size = new System.Drawing.Size(317, 29);
+			this.textbox1.TabIndex = 0;
+			//
+			// textbox2
+			//
+			this.textbox2.Location = new System.Drawing.Point(245, 142);
+			this.textbox2.Name = "textbox2";
+			this.textbox2.Size = new System.Drawing.Size(317, 29);
+			this.textbox2.TabIndex = 3;
+			this.textbox2.ReadOnly = true;
+			
+			//
 			// label1
-			// 
-			this.label1.Location = new System.Drawing.Point(43, 95);
+			//
+			this.label1.Location = new System.Drawing.Point(43, 82);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(180, 39);
-			this.label1.TabIndex = 7;
-			this.label1.Text = "Customer ID";
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Argument";
 			this.label1.Click += new System.EventHandler(this.Label1Click);
-			// 
+			//
 			// label4
-			// 
-			this.label4.Location = new System.Drawing.Point(43, 352);
+			//
+			this.label4.Location = new System.Drawing.Point(43, 142);
 			this.label4.Name = "label4";
 			this.label4.Size = new System.Drawing.Size(180, 39);
 			this.label4.TabIndex = 10;
-			this.label4.Text = "Country";
-			// 
-			// CustomerForm
-			// 
+			this.label4.Text = "Result";
+			//
+			// button1
+			//
+			this.button1.Location = new System.Drawing.Point(26, 22);
+			this.button1.Margin = new System.Windows.Forms.Padding(6);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(182, 42);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Start Calculation";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			//
+			// Form1
+			//
 			this.AutoScaleBaseSize = new System.Drawing.Size(9, 22);
-			this.ClientSize = new System.Drawing.Size(1240, 508);
+			this.ClientSize = new System.Drawing.Size(611, 211);
 			this.Controls.Add(this.label4);
 			this.Controls.Add(this.label1);
-			this.Controls.Add(this.txtCountry);
-			this.Controls.Add(this.txtCustID);
-			this.Name = "CustomerForm";
+			this.Controls.Add(this.textbox2);
+			this.Controls.Add(this.textbox1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
 			this.Text = "Customer Form";
 			this.ResumeLayout(false);
 			this.PerformLayout();
--- csharp/basic-countermonitor/Client/Form1.cs
+++ csharp/basic-countermonitor/Client/Form1.cs
@@ -1,20 +1,38 @@
 using System;
 using System.Drawing;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
 using System.Collections;
 using System.Windows.Forms;
 using System.Data;
+using System.Linq;
+using System.Threading;
 using System.Text;
 using System.IO;
 using System.ComponentModel;
+using Utils;
 
 namespace Program {
 
 	public partial class Form1 : Form {
 
-		public Form1() {
-			InitializeComponent();
-		}
+		private NameValueCollection appSettings;
+		private Discover discover1 = null;
+		private int interval = 100;
+		private int cnt = 0;
+		private string argument;
 
+		public Form1() {
+			appSettings = ConfigurationManager.AppSettings;
+			if (appSettings.AllKeys.Contains("Argument")) {
+				argument = appSettings["Argument"];
+			}
+
+			InitializeComponent();
+			this.textbox1.Text = argument;
+
+		}
 
 		[STAThread]
 		static void Main() {
@@ -22,7 +40,23 @@ namespace Program {
 		}
 
 		void Label1Click(object sender, EventArgs e) {
-	
+		}
+
+		private void button1_Click(object sender, EventArgs e) {
+			button1.safeInvoke((Control control) => control.Enabled = false);
+			textbox2.safeInvoke((TextBox textbox) => textbox.Text = "");
+
+			Func<string, string> getResult = (string arg) => {
+				cnt++;
+				Console.Error.WriteLine("cnt :" + cnt);
+				return cnt == 10 ? "DONE" : "";	};
+			discover1 = new Discover(  interval, getResult,argument);
+			cnt = 0;
+			discover1.startPollingForResult();
+		
+			Thread.Sleep(2500);
+			textbox2.safeInvoke((TextBox textbox) => textbox.Text = discover1.Result);
+			button1.safeInvoke((Control control) => control.Enabled = true);
 		}
 
 	}
