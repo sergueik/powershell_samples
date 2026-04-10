ce6b249a2282fabd33a759d0db0550dbecf4b9cb
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2026-04-09 20:31:38 -0400

    intemediate revision - cosmetic changes, adding label, some SharPoit Developer/Designer issue, unfinished

--- external/csharp/basic-progress-circular-osx/Program/Form1.cs
+++ external/csharp/basic-progress-circular-osx/Program/Form1.cs
@@ -16,8 +16,8 @@ using Utils;
 
 namespace Program {
 	public partial class Form1 : Form {
-		private CircularProgressControl cbc1;
-		private CircularProgressControl cbc2;
+		private CircularProgressControl circularProgressControl1;
+		private CircularProgressControl circularProgressControl2;
 		private Button button1;
 
 		private CircularBuffer<Data> buffer;
@@ -54,7 +54,7 @@ namespace Program {
 			this.Text = "OS X Progress Control";
 			this.BackColor = Color.LightGray;
 
-			cbc1 = new CircularProgressControl {
+			circularProgressControl1 = new CircularProgressControl {
 				Location = new Point(10, 20),
 				MinimumSize = new Size(56, 56),
 				Size = new Size(56, 56),
@@ -63,7 +63,7 @@ namespace Program {
 				StartAngle = 270
 			};
 
-			cbc2 = new CircularProgressControl {
+			circularProgressControl2 = new CircularProgressControl {
 				Location = new Point(10, 80),
 				MinimumSize = new Size(56, 56),
 				Size = new Size(56, 56),
@@ -79,16 +79,16 @@ namespace Program {
 			};
 			button1.Click += Button1_Click;
 
-			this.Controls.Add(cbc1);
-			this.Controls.Add(cbc2);
+			this.Controls.Add(circularProgressControl1);
+			this.Controls.Add(circularProgressControl2);
 			this.Controls.Add(button1);
 		}
 
 		private void Button1_Click(object sender, EventArgs e) {
 			if (button1.Text == "Start") {
 				button1.Text = "Stop";
-				cbc1.Start();
-				cbc2.Start();
+				circularProgressControl1.Start();
+				circularProgressControl2.Start();
 				timer1.Interval = collectInterval;
 				timer1.Enabled = true;
 				timer1.Elapsed += new ElapsedEventHandler((object source, ElapsedEventArgs args) => CollectMetrics());
@@ -100,8 +100,8 @@ namespace Program {
 				timer2.Start();  
 			} else {
 				button1.Text = "Start";
-				cbc1.Stop();
-				cbc2.Stop();
+				circularProgressControl1.Stop();
+				circularProgressControl2.Stop();
 				if (timer2 != null) {
 					timer1.Stop();
 					timer1.Enabled = false;
