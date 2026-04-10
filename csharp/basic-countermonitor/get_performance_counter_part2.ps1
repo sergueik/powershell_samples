b65524c2d24de16aeda91f3780699baebd62e46d
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2026-04-10 16:52:52 -0400

    intermediate revision - working Powershell POC

--- csharp/basic-countermonitor/get_performance_counter_part2.ps1
+++ csharp/basic-countermonitor/get_performance_counter_part2.ps1
@@ -33,19 +33,39 @@ if ($debug_flag){
 Add-Type -TypeDefinition @"
 
 // "
-
-using System;
 using System.Collections.Generic;
-using System.ComponentModel;
+using System.Collections;
+using System.Diagnostics;
 using System.Drawing;
-using System.Data;
-using System.Text;
-using System.Windows.Forms;
+using System.Linq;
 
-namespace ProgressControl
+using System.Timers;
+using System.Windows.Forms;
+using System;
+
+namespace Utils
 {
     public partial class CircularProgressControl : UserControl
     {
+
+        private string result;
+		public string Result { get { 
+			Console.Error.WriteLine(String.Format("result: {0}", this.result));
+			return result;}
+		}
+		private CircularBuffer<Data> buffer;
+
+		private System.Timers.Timer timer1;
+		private System.Timers.Timer timer2;
+
+		private int averageInterval = 30000;
+		private int collectInterval = 1000;
+		private static int capacity = 900;
+		// NOTE: the default value os
+		// categoryNAme, counterName and instanceName about to be overwrittedn my app config values
+		private string categoryName = "Memory";
+		private string counterName = "Available Bytes";
+		private string instanceName = "";
 
         private const int DEFAULT_INTERVAL = 60;
         private readonly Color DEFAULT_TICK_COLOR = Color.FromArgb(58, 58, 58);
@@ -73,7 +93,7 @@ namespace ProgressControl
         int m_SpokesCount = 0;
         int m_AngleIncrement = 0;
         int m_AlphaDecrement = 0;
-        Timer m_Timer = null;
+        System.Windows.Forms.Timer m_Timer = null;
 
         public int Interval
         {
@@ -145,7 +165,7 @@ namespace ProgressControl
             m_Pen = new Pen(TickColor, DEFAULT_TICK_WIDTH);
             m_Pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
             m_Pen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
-            m_Timer = new Timer();
+            m_Timer = new System.Windows.Forms.Timer();
             m_Timer.Interval = this.Interval;
             m_Timer.Tick += new EventHandler(OnTimerTick);
         }
@@ -233,11 +253,30 @@ namespace ProgressControl
 
         public void Start()
         {
+        				
+			timer1 = new System.Timers.Timer();
+			timer2 = new System.Timers.Timer();
+
+        	buffer = new CircularBuffer<Data>(capacity);
+
             if (m_Timer != null)
             {
                 m_Timer.Interval = this.Interval;
                 m_Timer.Enabled = true;
             }
+            // planted the code responsible for metric collection
+            
+				timer1.Interval = collectInterval;
+				timer1.Enabled = true;
+				timer1.Elapsed += new ElapsedEventHandler((object source, ElapsedEventArgs args) => CollectMetrics());
+				timer1.Start();
+
+				timer2.Interval = averageInterval;
+				timer2.Elapsed += new ElapsedEventHandler((object source, ElapsedEventArgs args) => Commit());
+				timer2.Enabled = true;
+				timer2.Start();
+	
+            
         }
 
         public void Stop()
@@ -246,11 +285,373 @@ namespace ProgressControl
             {
                 m_Timer.Enabled = false;
             }
+			if (timer2 != null) {
+					timer1.Stop();
+					timer1.Enabled = false;
+				}
+				if (timer2 != null) {
+					timer2.Stop();
+					timer2.Enabled = false;
+				}
+            
         }
-    }
+ 		private void CollectMetrics() {
+			float value = 0;
+			var row = new Data();
+			row.TimeStamp = DateTime.Now;
+			try {
+				// https://learn.microsoft.com/en-us/windows/win32/perfctrs/performance-counters-reference
+				var performanceCounter = new PerformanceCounter();
+				performanceCounter.CategoryName = this.categoryName;
+				performanceCounter.CounterName = this.counterName;
+				performanceCounter.InstanceName = instanceName == "" ? null : instanceName;
+				// value = (long)performanceCounter.RawValue;
+				value = performanceCounter.NextValue();
+			} catch (InvalidOperationException e) {
+				Console.Error.WriteLine(String.Format("Exception reading \"{0}\\{1}\\{2}\": {3}", categoryName, counterName, "0", e.ToString()));
+				return;
+			}
+			row.Value = value;
+			buffer.AddLast(row);
+		}
+        
+		private void Commit() {
+
+			var rows = buffer.ToList();
+			var now = DateTime.Now;
+			double average = 0;
+			IEnumerable<float> values = null;
+
+			try {
+				values = (from row in rows
+				          where ((now - row.TimeStamp).TotalMilliseconds) <= (float)this.averageInterval
+				          select row.Value);
+				average = values.Average();
+				this.result = String.Format("{0} from {1} samples", average, values.Count());
+				Console.Error.WriteLine(this.result);
+
+			} catch (Exception e) {
+				// System.InvalidOperationException: Sequence contains no elements
+				Console.Error.WriteLine(String.Format("Exception: {0}", e.ToString()));
+			}
+		}
+   }
+	public class Data{
+		public DateTime TimeStamp { get; set; }
+		public float Value { get; set; }
+		public override string ToString() {
+			return string.Format("TimeStamp={0} {1}, Value={2}", TimeStamp.ToLongDateString(), TimeStamp.ToLongTimeString(), Value);
+		}
+	}
+
+	public class CircularBuffer<T> : IEnumerator<T>
+	{
+		internal T[] _array;
+		internal Int32 _start;
+		internal Int32 _end;
+		private Int32 _size;
+		private Boolean _isDynamic;
+		private Boolean _isInfinite;
+		private Int32 _currentPosition = -1;
+		private Int32 _numOfElementsProcessed = 0;
+		public CircularBuffer()
+			: this(0, true, false)
+		{
+		}
+
+		public CircularBuffer(Int32 size)
+			: this(size, true, false)
+		{
+		}
+
+		public CircularBuffer(Int32 size, Boolean isInfinite, Boolean isDynamic)
+		{
+			_array = new T[size];
+			_start = 0;
+			_end = 0;
+			_size = 0;
+			_isDynamic = isDynamic;
+			_isInfinite = isInfinite;
+		}
+
+		public void AddFirst(T data)
+		{
+			if (!IsFull || !_isInfinite) {
+				if (_size != 0) {
+					_start = GetPreviousPosition(_start);
+					if (_start == _end) {
+						_end = GetNextPosition(_end);
+					} else {
+						_size++;
+					}
+				} else {
+					_start = 0;
+					_size++;
+				}
+				_array[_start] = data;
+			} else if (_isDynamic) {
+				Extend();
+				AddFirst(data);
+			} else {
+				throw new Exception("Array is Full");
+			}
+		}
+
+		public void AddLast(T data)
+		{
+			if (!IsFull || _isInfinite) {
+				if (_size != 0) {
+					_end = GetNextPosition(_end);
+					if (_start == _end) {
+						_start = GetNextPosition(_start);
+					} else {
+						_size++;
+					}
+				} else {
+					_end = 0;
+					_size++;
+				}
+				_array[_end] = data;
+			} else if (_isDynamic) {
+				Extend();
+				AddLast(data);
+			} else {
+				throw new Exception("Array is Full");
+			}
+		}
+
+		public void RemoveFirst()
+		{
+			if (!IsEmpty) {
+				_start = GetNextPosition(_start);
+				_size--;
+			} else {
+				throw new Exception("Array is Empty");
+			}
+		}
+
+		public void RemoveLast()
+		{
+			if (!IsEmpty) {
+				_end = GetPreviousPosition(_end);
+				_size--;
+			} else {
+				throw new Exception("Array is Empty");
+			}
+		}
+
+		private void Extend()
+		{
+			// The name 'Console' does not exist in the current context (CS0103)
+			// Console.WriteLine("Extening to Capacity: " + Capacity * 2 );
+			Extend(Capacity * 2);
+		}
+
+		private void Extend(Int32 capacity)
+		{
+			T[] extendedArray = new T[capacity];
+			if (capacity < Capacity) {
+				Int32 end = 0;
+				Int32 size = 0;
+				for (Int32 i = 0; i < extendedArray.Length && i < _array.Length; i++, end = GetNextPosition(end), size++) {
+					extendedArray[i] = _array[end];
+				}
+				_start = 0;
+				_end = --end;
+				_size = size;
+
+			} else {
+				_array.CopyTo(extendedArray, 0);
+			}
+			_array = extendedArray;
+		}
+
+		private Int32 GetNextPosition(Int32 pos)
+		{
+
+			if (pos == Capacity - 1) {
+				return 0;
+			} else {
+				return pos + 1;
+			}
+		}
+
+		private Int32 GetPreviousPosition(Int32 pos)
+		{
+			if (pos == 0) {
+				return Capacity - 1;
+			} else {
+				return pos - 1;
+			}
+		}
+
+		void IDisposable.Dispose()
+		{
+			_currentPosition = -1;
+			_numOfElementsProcessed = 0;
+		}
+
+		public bool MoveNext()
+		{
+			if (_currentPosition == -1) {
+				_currentPosition = _start;
+				_numOfElementsProcessed++;
+			} else {
+				_currentPosition = GetNextPosition(_currentPosition);
+				_numOfElementsProcessed++;
+			}
+			return (_numOfElementsProcessed <= Size);
+		}
+
+		public void Reset()
+		{
+			_start = 0;
+			_end = 0;
+			_size = 0;
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			return this;
+		}
+
+		public T this[Int32 index] {
+			get {
+				if (index >= 0 && index < _size) {
+					Int32 iBuffer = _start;
+					for (Int32 incremented = 0; incremented < index; incremented++) {
+						iBuffer = GetNextPosition(iBuffer);
+					}
+					return _array[iBuffer];
+				} else {
+					throw new Exception("Invalid Index");
+				}
+			}
+			set {
+				if (index >= 0 && index <= _size) {
+					Int32 iBuffer = _start;
+					for (Int32 incremented = 0; incremented < index; incremented++) {
+						iBuffer++;
+					}
+					_array[iBuffer] = value;
+				} else {
+					throw new Exception("Invalid Index");
+				}
+			}
+		}
+
+		public T[] ToArray()
+		{
+			Int32 numOfElementsProcessed = 0;
+			T[] array = new T[_size];
+			Int32 iOutputArray = 0;
+			for (Int32 iBuffer = _start; numOfElementsProcessed < _size; iBuffer = GetNextPosition(iBuffer), numOfElementsProcessed++, iOutputArray++) {
+				array[iOutputArray] = _array[iBuffer];
+			}
+			return array;
+		}
+
+		public List<T> ToList()
+		{
+			Int32 numOfElementsProcessed = 0;
+			var list = new List<T>();
+			for (Int32 iBuffer = _start; numOfElementsProcessed < _size; iBuffer = GetNextPosition(iBuffer), numOfElementsProcessed++) {
+				list.Add(_array[iBuffer]);
+			}
+			return list;
+		}
+
+		protected Int32 Start {
+			set {
+				if (value >= 0 && value < _array.Length) {
+					_start = value;
+				} else {
+					throw new Exception("Inappropriate Index");
+				}
+			}
+			get {
+				return _start;
+			}
+		}
+
+		protected Int32 End {
+			set {
+				if (value >= 0 && value < _array.Length) {
+					_end = value;
+				} else {
+					throw new Exception("Inappropriate Index");
+				}
+			}
+			get {
+				return _end;
+			}
+		}
+
+		public Int32 Size {
+			get {
+				return _size;
+			}
+		}
+
+		public Int32 Capacity {
+			set {
+				Extend(value);
+			}
+			get {
+				return _array.Length;
+			}
+		}
+
+		public Boolean IsDynamic {
+			set {
+				_isDynamic = value;
+			}
+			get {
+				return _isDynamic;
+			}
+		}
+
+		public Boolean IsInfinite {
+			set {
+				_isInfinite = value;
+			}
+			get {
+				return _isInfinite;
+			}
+		}
+
+		private Boolean IsEmpty {
+			get {
+				return _size == 0;
+			}
+		}
+
+		private Boolean IsFull {
+			get {
+				return _size == Capacity;
+			}
+		}
+
+		object IEnumerator.Current {
+			get {
+				return Current;
+			}
+		}
+
+		public T Current {
+			get {
+				try {
+					return _array[_currentPosition];
+				} catch (IndexOutOfRangeException) {
+					throw new InvalidOperationException();
+				}
+			}
+		}
+	}	
 }
 
-"@ -ReferencedAssemblies 'System.Windows.Forms.dll','System.Drawing.dll','System.Data.dll'
+
+"@ -ReferencedAssemblies 'System.Windows.Forms.dll','System.Drawing.dll','System.Data.dll', 'System.Collections.dll', 'System.xml.dll','System.Xml.Linq.dll'
 
 
 
@@ -313,18 +714,25 @@ $f = New-Object System.Windows.Forms.Form
 $f.AutoScaleDimensions = New-Object System.Drawing.SizeF (6.0,13.0)
 $f.AutoScaleMode = [System.Windows.Forms.AutoScaleMode]::Font
 $f.BackColor = [System.Drawing.Color]::LightGray
-$f.ClientSize = New-Object System.Drawing.Size (170,140)
+$f.ClientSize = New-Object System.Drawing.Size (257,119)
 
 $button1 = New-Object System.Windows.Forms.Button
-$cbc1 = New-Object ProgressControl.CircularProgressControl
-$cbc2 = New-Object ProgressControl.CircularProgressControl
+$label = New-Object System.Windows.Forms.Label
+$cbc1 = New-Object Utils.CircularProgressControl
 $f.SuspendLayout()
 
-$button1.Location = New-Object System.Drawing.Point (70,80)
-$button1.Name = "button1"
+$label.AutoSize = $true
+$label.Location = new-object System.Drawing.Point(20, 78)
+$label.Margin = new-object System.Windows.Forms.Padding(6, 0, 6, 0)
+$label.Name = "label1"
+$label.Size = new-object System.Drawing.Size(224, 27)
+$label.TabIndex = 2
+
+$button1.Location = New-Object System.Drawing.Point (170,40)
+$button1.Name = 'button1'
 $button1.Size = New-Object System.Drawing.Size (75,23)
 $button1.TabIndex = 0
-$button1.Text = "Start"
+$button1.Text = 'Start'
 $button1.UseVisualStyleBackColor = true
 $button1.add_click.Invoke({
     param(
@@ -335,13 +743,11 @@ $button1.add_click.Invoke({
     {
       $button1.Text = 'Stop'
       $cbc1.Start()
-      $cbc2.Start()
     }
     else
     {
       $button1.Text = 'Start'
       $cbc1.Stop()
-      $cbc2.Stop()
     }
   })
 
@@ -357,22 +763,11 @@ $cbc1.StartAngle = 270
 $cbc1.TabIndex = 1
 $cbc1.TickColor = [System.Drawing.Color]::DarkBlue
 
-$cbc2.BackColor = [System.Drawing.Color]::Transparent
-$cbc2.Interval = 60
-$cbc2.Location = New-Object System.Drawing.Point (10,80)
-$cbc2.MinimumSize = New-Object System.Drawing.Size (56,56)
-$cbc2.Name = "$cbc2"
-$cbc2.Clockwise = $false
-$cbc2.Size = New-Object System.Drawing.Size (56,56)
-$cbc2.StartAngle = 270
-$cbc2.TabIndex = 2
-$cbc2.TickColor = [System.Drawing.Color]::Yellow
-
-$f.Controls.Add($cbc2)
+$f.Controls.Add($label)
 $f.Controls.Add($button1)
 $f.Controls.Add($cbc1)
-$f.Name = "Form1"
-$f.Text = 'OS X Progress Control'
+$f.Name = 'Form1'
+$f.Text = 'Progress Control'
 $f.ResumeLayout($false)
 
 [void]$f.ShowDialog()
