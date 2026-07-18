8eeee0ff6c83fa17df28d88ddc6cb8316ee89e97
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2026-07-18 10:55:10 -0400

    intermediate revision - match standard WinForms rendering pipeline to perform scaling

--- external/csharp/basic-virtualbox-longrunningcommand/Utils/CustomDisabledIconRenderer.cs
+++ external/csharp/basic-virtualbox-longrunningcommand/Utils/CustomDisabledIconRenderer.cs
@@ -14,20 +14,30 @@ namespace Utils {
 				return;
 			}
 
-			// Tag != null manifests "draw using our custom disabled appearance".
+			// Tag != null indicates that this item should use custom disabled appearance
 			if (e.Item.Tag != null) {
 				// TODO: intriduce enum
 				// VmInfo vm = e.Item.Tag as VmInfo;
 				// if (vm != null && vm.State == VmState.Saved)
-				// NOTE: oncorrectly scaled on X
-				Debug.WriteLine(String.Format("Image size: {0} ImageRectangle: {1} ", e.Image.Size, e.ImageRectangle));
+				// NOTE: does not scale the image bitmap
 				// https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.controlpaint.drawimagedisabled?view=netframework-4.5
-				ControlPaint.DrawImageDisabled(e.Graphics, e.Image, e.ImageRectangle.X, e.ImageRectangle.Y, e.Item.BackColor);
+				if (e.Image.Size.Width == e.ImageRectangle.Width && e.Image.Size.Height == e.ImageRectangle.Height)
+					ControlPaint.DrawImageDisabled(e.Graphics, e.Image, e.ImageRectangle.X, e.ImageRectangle.Y, e.Item.BackColor);
+				else {
+					Debug.WriteLine(String.Format("Image size: {0} ImageRectangle: {1} ", e.Image.Size, e.ImageRectangle));
+					using (var scaledBitmap = new Bitmap(e.Image, e.ImageRectangle.Width, e.ImageRectangle.Height)) {
+						ControlPaint.DrawImageDisabled( e.Graphics, scaledBitmap, e.ImageRectangle.X, e.ImageRectangle.Y, e.Item.BackColor);
+					}
+				}
+				// Will dispose the temporary bitmap automatically
 				return;
 			}
 
-			// Fallback to default rendering.
+			// Fallback to the standard WinForms ToolStripProfessionalRenderer
+			// implementation (image scaling, positioning and normal rendering).
 			// https://learn.microsoft.com/en-us/dotnet/api/system.drawing.graphics.drawimage?view=netframework-4.5
+			// https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.toolstriprenderer.onrenderitemimage?view=netframework-4.5
+			// https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.toolstripprofessionalrenderer?view=netframework-4.5
 			base.OnRenderItemImage(e);
 		}
 	}
