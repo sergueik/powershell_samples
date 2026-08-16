85774f1df66f9770a36eb20fffd116ff01a916b9
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2026-08-16 10:28:54 -0400

    refactored dictionary capturing regex string extension method test with date formats

--- external/csharp/basic-renamer/Test/ExtensionTest.cs
+++ external/csharp/basic-renamer/Test/ExtensionTest.cs
@@ -8,9 +8,11 @@ using NUnit.Framework;
 
 using Utils;
 
-namespace Test {
+namespace Test
+{
 	[TestFixture]
-	public class ExtensionTest {
+	public class ExtensionTest
+	{
 		// NOTE: unused
 		private const string name = @"(01) Artist Title";
 		private const string matchPattern = @"\\((?<index>[AB0-9][0-9]+)\\)\\s+(?<artist>[^ ].+[^ ])\\s+(?<title>[^ ].+)";
@@ -23,31 +25,46 @@ namespace Test {
 		}
 
 		[TestFixtureTearDown]
-		public static void Cleanup() { }
+		public static void Cleanup()
+		{
+		}
 
 		[Test]
 		public void test() {
-			var date = "2026-Aug 18";
-			var dateDattern = @"\d{4}\-(?<month>\w{3})";
-			var dateDatternReg = new Regex(dateDattern);
-			Assert.IsTrue(dateDatternReg.IsMatch(date));
-			String month = date.FindMatch(dateDattern);
-			Console.WriteLine(String.Format("month:{0}", month));
-			var dictionary = date.FindMatches(dateDattern);
+			
+			var date = DateTime.Today;
+
+			var us = date.ToString("MM/dd/yyyy");
+			var eu = date.ToString("dd/MM/yyyy");
+
+			var dateDattern = @"(?<month>\d{2})/(?<day>\d{2})/(?<year>\d{4})";
+
+			Assert.IsTrue(Regex.IsMatch(us, "^" + dateDattern + "$"));
+
+			var dictionary = us.FindMatches(dateDattern);
 			Assert.NotNull(dictionary);
+			// novel NUnit 3.0 features - That, Does
+			// Assert.That(dictionary.Keys, Does.Contain("month"));
 			Assert.Contains("month", dictionary.Keys);
 			Assert.NotNull(dictionary["month"]);
+
+			Assert.AreEqual(date.Month.ToString("00"), dictionary["month"]);
+			Assert.AreEqual(date.Day.ToString("00"), dictionary["day"]);
+			Assert.AreEqual(date.Year.ToString(), dictionary["year"]);
+
 			var resultRegex = new Regex("<([^>]+)>", RegexOptions.IgnoreCase | RegexOptions.Compiled);
-			var check = "the month is <month>";
-			Console.WriteLine(String.Format("result:{0}", resultRegex.Replace(check, (Match match) => {
+			var check = "<day>/<month>/<year>";
+			var result = resultRegex.Replace(check, (Match match) => {
 				string key = match.Groups[1].Value;
-				// Return dictionary value if it exists, otherwise keep original match
+				// Return dictionary value resultif it exists, otherwise keep original match
 				string value;
 				return dictionary.TryGetValue(key, out value) ? value : match.Value;
-			})));
+			});
+			Assert.AreEqual(eu, result);
 			// one cannot pass a dictionary lookup directly like dictionary["$1"] inside Regex.Replace. $1 is a regex replacement token string, not a live variable or group value evaluated at runtime
 			// System.Collections.Generic.KeyNotFoundException : The given key was not present in the dictionary
 			Assert.Throws<KeyNotFoundException>(() => resultRegex.Replace(check, dictionary["$1"]));
+			// the dictionary["$1"] doesn't invoke any regex machinery at all. It literally means: look up the string $1 in this dictionary.
 		}
 
 	}
